4d1477bbbcec5e14d8769511ae4c8af3bee4eb36
wszyt <173982112@qq.com>
2019-04-02 22:19:28 +0800

    int commit

--- src/main/java/com/zyt/myshop/service/user/consumer/MyShopServiceUserConsumerApplication.java
+++ src/main/java/com/zyt/myshop/service/user/consumer/MyShopServiceUserConsumerApplication.java
@@ -2,10 +2,13 @@ package com.zyt.myshop.service.user.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@SpringBootApplication(scanBasePackages = "com.zyt.myshop")
+import javax.sql.DataSource;
+
+@SpringBootApplication(scanBasePackages = "com.zyt.myshop", exclude = DataSourceAutoConfiguration.class)
 @EnableHystrix
 @EnableHystrixDashboard
 public class MyShopServiceUserConsumerApplication {
